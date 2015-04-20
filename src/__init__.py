12122054b70fb047cf69cafe55171723fa6eee3d
Haochen Wu <wuhaochen42@gmail.com>
2015-04-20 13:54:36 -0700

    change current dirc when load

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 
+working_path = '/home/hcwu/work/research/airnet'
+
+import os
+
+os.chdir(working_path)
+
 from airnet import *
-from measures import *
 from filters import *
 from weight import *
 from helper import *
+from count import *
+from aggregate import *
+
 from main import *
