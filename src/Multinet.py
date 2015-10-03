fc043aa9893293bb87e596d7def1a35acbe07277
Haochen Wu <wuhaochen42@gmail.com>
2015-10-02 19:00:37 -0700

    Simple functions

--- src/Multinet.py
+++ src/Multinet.py
@@ -9,5 +9,18 @@ class Multinet(nx.DiGraph):
         super(self.__class__,self).__init__()
         self.graph['layers'] = []
 
-    def layers():
+    def _add_layer(self,layer):
+        if layer not in self.layers():
+            self.graph['layers'].append(layer)
+        
+    def layers(self):
         return self.graph['layers']
+
+    def add_edge(self, u, v, layer, weight=1.0):
+        super(self.__class__,self).add_edge(u,v)
+
+        self._add_layer(layer)
+            
+        if not self[u][v].has_key('multiplex'):
+            self[u][v]['multiplex'] = {}
+        self[u][v]['multiplex'][layer] = weight
