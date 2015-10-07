5a1cda7727b38c2f5bd836ca7c819636aeb049fd
Haochen Wu <wuhaochen42@gmail.com>
2015-10-07 12:03:35 -0700

    add trimed mg

--- src/example.py
+++ src/example.py
@@ -1,5 +1,6 @@
 import util
 import builder
+import networkx as nx
 
 from params import default_path
 
@@ -9,3 +10,12 @@ def mg_by_year(year):
     weightf = util.weight_from_string('PASSENGERS')
     layerf = util.layer_from_string('CARRIER')
     return builder.multinet_from_csv(airfile,filt,weightf,'CARRIER')
+
+def trimed_mg_by_year(year):
+    mg = mg_by_year(year)
+    c = list(nx.strongly_connected_components(mg))
+    c = sorted(c,key=len,reverse=True)
+    to_remove = set(mg.nodes())-set(c[0])
+    mg.remove_nodes_from(to_remove)
+    mg.remove_empty_layers()
+    return mg
