713a43c5d949f3de81a3b9ed38966a9ee3ac3d3d
Haochen Wu <wuhaochen42@gmail.com>
2017-09-27 11:56:12 -0700

    Add dependency to setup

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
           author='Haochen Wu',
           author_email='multinet@haochenwu.com',
           packages=packages,
-          package_dir=package_dir
+          package_dir=package_dir,
+          install_requires=['networkx','dit']
     )
 
