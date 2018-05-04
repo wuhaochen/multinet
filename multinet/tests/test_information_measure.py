453f236622e3ec998766f8448ea467c0716c0f06
Haochen Wu <wuhaochen42@gmail.com>
2018-05-04 14:48:31 -0700

    refactor and add tests

---
+++ multinet/tests/test_information_measure.py
@@ -0,0 +1,36 @@
+import multinet as mn
+import networkx as nx
+
+g1 = nx.Graph()
+nodes = ['A', 'B', 'C', 'D', 'E']
+edges = [('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'E'), ('E', 'A')]
+g1.add_nodes_from(nodes)
+g1.add_edges_from(edges)
+
+g2 = nx.Graph()
+nodes = ['A', 'B', 'C', 'D', 'E']
+edges = [('A', 'C'), ('B', 'D'), ('C', 'E'), ('D', 'A'), ('E', 'B')]
+g2.add_nodes_from(nodes)
+g2.add_edges_from(edges)
+
+g3 = nx.Graph()
+nodes = ['A', 'B', 'C', 'D', 'E']
+edges = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E'), ('B', 'D'), ('C', 'E')]
+g3.add_nodes_from(nodes)
+g3.add_edges_from(edges)
+
+mg = mn.Multinet()
+
+mg.add_layer(g1, '1')
+mg.add_layer(g2, '2')
+mg.add_layer(g3, '3')
+
+
+class TestMutualInformation(object):
+
+
+    def test_mutual_infomation(self):
+        assert mn.extract_count(mg, ['1', '2'])['00'] == 0
+
+class TestTransferEntropy(object):
+    pass
---
+++ multinet/tests/test_random_models.py
@@ -0,0 +1,53 @@
+import random
+
+import multinet as mn
+import networkx as nx
+
+nlayers = 5
+nnodes = 20
+
+mg = mn.Multinet()
+for layer in range(nlayers):
+    p = random.uniform(0.3,0.6)
+    layer_name = '%d' % layer
+    g = nx.erdos_renyi_graph(nnodes, p)
+    mg.add_layer(g, layer_name)
+
+dmg = mn.DiMultinet()
+for layer in range(nlayers):
+    p = random.uniform(0.3,0.6)
+    layer_name = '%d' % layer
+    g = nx.erdos_renyi_graph(nnodes, p)
+    mg.add_layer(g, layer_name)
+
+
+class TestErdosRenyi(object):
+
+
+    def test_undirected(self):
+        seed = 42
+        
+        rmg = mn.multiplex_erdos_renyi(mg, seed=seed)
+        rmg = mn.multiplex_erdos_renyi(mg)
+
+
+    def test_directed(self):
+        seed = 42
+        
+        rmg = mn.multiplex_erdos_renyi(dmg, seed=seed)
+        rmg = mn.multiplex_erdos_renyi(dmg)
+
+
+class TestIndependentConfig(object):
+
+
+    def test_undirected(self):
+        seed = 42
+        rmg = mn.multiplex_configuration_independent(mg, seed=seed)
+        rmg = mn.multiplex_configuration_independent(mg)
+
+
+    def test_directed(self):
+        seed = 42
+        rmg = mn.multiplex_configuration_independent(dmg, seed=seed)
+        rmg = mn.multiplex_configuration_independent(dmg)
